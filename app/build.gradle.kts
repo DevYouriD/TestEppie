ddeadecf7bdc20955c0b3ed03e0a3d3f1820530e
peppe0203 <gcollura2001@gmail.com>
2023-08-28 19:32:29 +0200

    Added basic navigation menu.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -62,6 +65,8 @@ dependencies {
     // To use constraintlayout in compose
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    // Used for navigation drawer
+    implementation("com.google.android.material:material:1.9.0")
 
     // TEST
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/testeppie/MainActivity.kt
+++ app/src/main/java/com/example/testeppie/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.testeppie
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var counterTextView: TextView
@@ -15,10 +18,42 @@ class MainActivity : AppCompatActivity() {
 
     private var counter = 0
 
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Drawer menu items
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // ?. is a null check (Elvise operator)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navigationView = findViewById<NavigationView>(R.id.navView)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuItem1 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked item 1",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                R.id.menuItem2 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked item 2",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            // True because we handled the click
+            true
+        }
+
+        // Basic logic for the test app counter
         counterTextView = findViewById(R.id.textView5)
         increaseButton = findViewById(R.id.increaseButton)
         decreaseButton = findViewById(R.id.decreaseButton)
@@ -38,6 +73,17 @@ class MainActivity : AppCompatActivity() {
             counter = 0
             counterTextView.text = counter.toString()
         }
+    }
+
+    /**
+     * Used to response correctly on click on the toggle button and the clicks
+     * on the menu items in the navigation drawer.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
